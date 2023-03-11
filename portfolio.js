620e0ec2bc4f76f7dfe153d375cba5de34247776
Pabl0Parra <pabsblue16@gmail.com>
2023-03-11 09:18:47 +0100

    udate desc for JobMatch

--- portfolio.js
+++ portfolio.js
@@ -243,7 +243,7 @@ export const projects = [
     name: "JobMatch",
     image:
       "https://res.cloudinary.com/dcwx2biey/image/upload/v1677687347/login_ccir9u.jpg",
-    desc: "This is the landing page for JobMatch, a job search app that I develop with React Native.",
+    desc: "This is the landing page for JobMatch, a job search app that I develop with React Native. The apk is available on the landing page.",
     github: "https://github.com/Pabl0Parra/JobMatch",
     link: "https://getjobmatch.netlify.app/",
   },
